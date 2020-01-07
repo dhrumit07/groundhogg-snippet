a02e2af5b4ca184532897e0767d30bb3ce35d6a5
Dhrumit07 <dhrumit07@gmail.com>
2020-01-07 11:05:33 -0500

    added custom test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,25 +1,25 @@
 <?php
-/**
- * PHPUnit bootstrap file
- *
- * @package Wp_Meta_Verify
- */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
-// Give access to tests_add_filter() function.
+define('PLUGIN_NAME','groundhogg-snippet.php');
+define('PLUGIN_FOLDER',basename(dirname( __DIR__ )));
+define('PLUGIN_PATH',PLUGIN_FOLDER.'/'.PLUGIN_NAME);
+
+// Activates this plugin in WordPress so it can be tested.
+$GLOBALS['wp_tests_options'] = array(
+  'active_plugins' => array( PLUGIN_PATH ),
+);
+
 require_once $_tests_dir . '/includes/functions.php';
 
-/**
- * Manually load the plugin being tested.
- */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/wp-meta-verify.php';
+	require dirname( __DIR__ ) . '/'.PLUGIN_NAME;
 }
+
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-// Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
